8660ea5753cd59280be705f9d1a665249431acaf
TrustBiz <60724108+ChenSeeSai@users.noreply.github.com>
2020-11-03 01:54:13 +0800

    route url

--- app/routes.py
+++ app/routes.py
@@ -14,14 +14,13 @@ from datatables import ColumnDT, DataTables
 from app.tasks.asin_task import save_data
 from app.forms.search import SearchForm
 
-host_url = 'http://127.0.0.1:5000'
 bp = Blueprint("all", __name__)
 app_root = os.path.dirname(os.path.abspath(__file__))
 
 @bp.route("/")
 def index():
     search_form = SearchForm()
-    return render_template('index.html', host_url=host_url, form=search_form, count=-1)
+    return render_template('index.html', form=search_form, count=-1)
 
 @bp.route('/uploader', methods=['GET', 'POST'])
 def uploader():
@@ -45,7 +44,7 @@ def uploader():
             save_data.delay(cc[0], cc[1])
             count += 1
 
-        return str(count) + ' crawlers are running in background, you can check them to refresh the page. <a href="/" class="btn btn-primary">Refresh</a>'
+        return str(count) + ' crawlers are running in background, you can check them to refresh the page.' + ' <a href="/">back</a>'
     else:
         redirect('/')
 
@@ -58,7 +57,7 @@ def search():
         result = query.all()
         return render_template('search_result.html', result=result)
 
-@bp.route('/excel/<string:from_date>/<string:to_date>', methods=['GET'])
+@bp.route('/to_excel/<string:from_date>/<string:to_date>', methods=['GET'])
 def to_excel(from_date, to_date):
     from_date = datetime.strptime(from_date, '%Y-%m-%d')
     to_date = datetime.strptime(to_date, '%Y-%m-%d') + timedelta(days=1)
@@ -74,6 +73,7 @@ def to_excel(from_date, to_date):
         df = df.append({'Amazon Site': row.site_url, 'ASIN': row.asin, 'Review Rating': row.review_rating,'Monteray Unit': row.unit,'Selling Price':row.sell_price,'Link': row.link,'Quantity of Reviews':str(row.quantity)},ignore_index=True)
 
     df.to_excel(filename,encoding='utf-8-sig',index=False)
+
     return send_file(filename)
 
 @bp.route('/get_data')
