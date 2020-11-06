291812d50ac97874928638ec9dbfc9db364ed57b
TrustBiz <60724108+ChenSeeSai@users.noreply.github.com>
2020-11-06 15:14:19 +0800

    add block exception case

--- app/helper.py
+++ app/helper.py
@@ -13,41 +13,62 @@ def crawler_result(site_url, asin):
 
     headersf={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
     req = requests.get(url,headers=headersf)
-
-    if req.status_code == 200:
-        s=BeautifulSoup(req.content,features="lxml")
-        quantr=""
-        try:
-            quantity_reviews=s.select("#acrCustomerReviewText.a-size-base")[0].get_text().strip()
-            quantity_reviews=quantity_reviews.replace("ratings","")
-            quantity_reviews = re.findall("(\d+(?:,d+)*)", quantity_reviews)
-
-            for x in range(len(quantity_reviews),0,-1):
-                quantr=str(quantity_reviews[x-1])+quantr
-        except IndexError:
-            quantr='N/A'
-
-        try:
-            review=s.select("#averageCustomerReviews")[0].get_text().strip()
-            review=str(review)
-            r=review.split()
-            review=r[0]
-            if(len(review)>4):
-                review=review[-3:]
-        except IndexError:
-            review='N/A'
-
-        if(review=='N/A'):
+    try:
+        if req.status_code == 200:
+            s=BeautifulSoup(req.content,features="lxml")
+            quantr=""
             try:
-                price=s.select("#acr-average-stars-rating-text")[0].get_text().strip()
+                quantity_reviews=s.select("#acrCustomerReviewText.a-size-base")[0].get_text().strip()
+                quantity_reviews=quantity_reviews.replace("ratings","")
+                quantity_reviews = re.findall("(\d+(?:,d+)*)", quantity_reviews)
+
+                for x in range(len(quantity_reviews),0,-1):
+                    quantr=str(quantity_reviews[x-1])+quantr
+            except IndexError:
+                quantr='N/A'
+
+            try:
+                review=s.select("#averageCustomerReviews")[0].get_text().strip()
+                review=str(review)
+                r=review.split()
+                review=r[0]
+                if(len(review)>4):
+                    review=review[-3:]
+            except IndexError:
+                review='N/A'
+
+            if(review=='N/A'):
+                try:
+                    price=s.select("#acr-average-stars-rating-text")[0].get_text().strip()
+                    if("-" in str(price)):
+                        a=str(price).split("-")
+                        f=a[0]
+                        g=a[1]
+                        price=f[1:]+"-"+g[2:]
+                        memontary_unit=f[0:1]
+                        memontary_unit=g[0:1]
+                    elif (" " in str(price)):
+                        a=str(price).split()
+                        price=a[0]
+                        memontary_unit=a[1]
+                    else:
+                        g=str(price)
+                        price=price[1:]
+                        memontary_unit=g[0:1]
+                except IndexError:
+                    price='N/A'
+                    memontary_unit='N/A'
+
+            try:
+                price=s.select("#priceblock_ourprice")[0].get_text().strip()
                 if("-" in str(price)):
                     a=str(price).split("-")
                     f=a[0]
                     g=a[1]
                     price=f[1:]+"-"+g[2:]
                     memontary_unit=f[0:1]
-                    memontary_unit=g[0:1]
-                elif (" " in str(price)):
+                    memontary_unit=f[0:1]
+                elif ("," in str(price)):
                     a=str(price).split()
                     price=a[0]
                     memontary_unit=a[1]
@@ -59,86 +80,53 @@ def crawler_result(site_url, asin):
                 price='N/A'
                 memontary_unit='N/A'
 
-        try:
-            price=s.select("#priceblock_ourprice")[0].get_text().strip()
-            if("-" in str(price)):
-                a=str(price).split("-")
-                f=a[0]
-                g=a[1]
-                price=f[1:]+"-"+g[2:]
-                memontary_unit=f[0:1]
-                memontary_unit=f[0:1]
-            elif ("," in str(price)):
-                a=str(price).split()
-                price=a[0]
-                memontary_unit=a[1]
-            else:
-                g=str(price)
-                price=price[1:]
-                memontary_unit=g[0:1]
-        except IndexError:
-            price='N/A'
-            memontary_unit='N/A'
+            if price=='N/A':
+                try:
+                    price=s.select("#priceblock_saleprice")[0].get_text().strip()
+                    if("-" in str(price)):
+                        a=str(price).split("-")
+                        f=a[0]
+                        g=a[1]
+                        price=f[1:]+"-"+g[2:]
+                        memontary_unit=f[0:1]
+                        memontary_unit=g[0:1]
+                    elif (" " in str(price)):
+                        a=str(price).split()
+                        price=a[0]
+                        memontary_unit=a[1]
+                    else:
+                        g=str(price)
+                        price=price[1:]
+                        memontary_unit=g[0:1]
+                except IndexError:
+                    price='N/A'
+                    memontary_unit='N/A'
 
-        if price=='N/A':
-            try:
-                price=s.select("#priceblock_saleprice")[0].get_text().strip()
-                if("-" in str(price)):
-                    a=str(price).split("-")
-                    f=a[0]
-                    g=a[1]
-                    price=f[1:]+"-"+g[2:]
-                    memontary_unit=f[0:1]
-                    memontary_unit=g[0:1]
-                elif (" " in str(price)):
-                    a=str(price).split()
-                    price=a[0]
-                    memontary_unit=a[1]
-                else:
-                    g=str(price)
-                    price=price[1:]
-                    memontary_unit=g[0:1]
-            except IndexError:
-                price='N/A'
-                memontary_unit='N/A'
+            if price=='N/A':
+                try:
+                    price=s.select("#priceblock_dealprice")[0].get_text().strip()
+                    if("-" in str(price)):
+                        a=str(price).split("-")
+                        f=a[0]
+                        g=a[1]
+                        price=f[1:]+"-"+g[2:]
+                        memontary_unit=f[0:1]
+                        memontary_unit=g[0:1]
+                    elif (" " in str(price)):
+                        a=str(price).split()
+                        price=a[0]
+                        memontary_unit=a[1]
+                    else:
+                        g=str(price)
+                        price=price[1:]
+                        memontary_unit=g[0:1]
+                except IndexError:
+                    price='N/A'
+                    memontary_unit='N/A'
 
-        if price=='N/A':
-            try:
-                price=s.select("#priceblock_dealprice")[0].get_text().strip()
-                if("-" in str(price)):
-                    a=str(price).split("-")
-                    f=a[0]
-                    g=a[1]
-                    price=f[1:]+"-"+g[2:]
-                    memontary_unit=f[0:1]
-                    memontary_unit=g[0:1]
-                elif (" " in str(price)):
-                    a=str(price).split()
-                    price=a[0]
-                    memontary_unit=a[1]
-                else:
-                    g=str(price)
-                    price=price[1:]
-                    memontary_unit=g[0:1]
-            except IndexError:
-                price='N/A'
-                memontary_unit='N/A'
-
-        if review == 'N/A' and memontary_unit == 'N/A' and quantr == 'N/A' and price == 'N/A':
-            res = {
-            'status': 'error',
-            'site_url': site_url,
-            'asin': asin,
-            'review': review,
-            'unit': memontary_unit,
-            'quantity': quantr,
-            'price': price,
-            'link': url,
-            'description': 'The cralwer has not gotten the correct data.'
-        }
-        else:
-            res = {
-                'status': 'success',
+            if review == 'N/A' and memontary_unit == 'N/A' and quantr == 'N/A' and price == 'N/A':
+                res = {
+                'status': 'error',
                 'site_url': site_url,
                 'asin': asin,
                 'review': review,
@@ -146,21 +134,44 @@ def crawler_result(site_url, asin):
                 'quantity': quantr,
                 'price': price,
                 'link': url,
-                'description': ''
+                'description': 'The cralwer has not gotten the correct data.'
             }
-    else:
+            else:
+                res = {
+                    'status': 'success',
+                    'site_url': site_url,
+                    'asin': asin,
+                    'review': review,
+                    'unit': memontary_unit,
+                    'quantity': quantr,
+                    'price': price,
+                    'link': url,
+                    'description': ''
+                }
+        else:
+            res = {
+                'status':  str(req.status_code) + ' Error code',
+                'site_url': site_url,
+                'asin': asin,
+                'review': '',
+                'unit': '',
+                'quantity': '',
+                'price': '',
+                'link': url,
+                'description': req.reason
+            }
+    except:
         res = {
-            'status':  str(req.status_code) + ' Error code',
-            'site_url': site_url,
-            'asin': asin,
-            'review': '',
-            'unit': '',
-            'quantity': '',
-            'price': '',
-            'link': url,
-            'description': req.reason
-        }
-
+                'status':  'Error code',
+                'site_url': site_url,
+                'asin': asin,
+                'review': '',
+                'unit': '',
+                'quantity': '',
+                'price': '',
+                'link': url,
+                'description': 'Blocked'
+            }
     return res
 
 def to_dict(row):
